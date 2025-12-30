097179ca31d61b7df53fc183c0521a0cae561331
Mark Day <markday922@gmail.com>
2025-12-30 15:17:50 +0000

    updated variables file

--- bastion-with-iap/variables.tf
+++ bastion-with-iap/variables.tf
@@ -8,6 +8,11 @@ variable "region" {
   type        = string
 }
 
+variable "zone" {
+  description = "Google Cloud availability zone"
+  type        = string
+}
+
 variable "vpc" {
   description = "Google Cloud vpc"
   type        = string
@@ -15,5 +20,10 @@ variable "vpc" {
 
 variable "bastion_name" {
   description = "Bastion Name"
+  type        = string
+}
+
+variable "service_account_name" {
+  description = "Bastion service account"
   type        = string
 }
